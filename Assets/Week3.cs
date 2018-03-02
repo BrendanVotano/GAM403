e0926100a319ba47fc0a9c8745949ae1aeef4404
BrendanVotano <brendan@blue-volcano.com>
2018-03-02 16:10:30 +1100

    Wek 3

---
+++ Assets/Week3.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Week3 : MonoBehaviour {
+
+	public int health;
+	public int mana;	//Mana is used by our magic system 
+	public float attackPower;
+	public float defencePower;
+
+	public GameObject player;
+
+	[Header("Enemy Variables")]
+	public GameObject[] enemies;
+	public int[] enemyHitPoints;
+
+	public int enemyNumb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		ResetCharacter ();
+	}
+
+	//Sets all our characters values to default
+	void ResetCharacter()
+	{
+		health = 100;
+		mana = 100;
+		attackPower = 10;
+		defencePower = 5;
+	}
+
+	void DecrementHealth()
+	{
+		health -= 10;
+	}
+
+	int MinusHealth(int power, int modifier)
+	{
+		return health -= (power * modifier);
+	}
+
+	void KillEnemy(int enemyNumber)
+	{
+		enemies [enemyNumber].SetActive (false);
+	}
+
+	void ShowEnemyName(int enemyNumber)
+	{
+		Debug.Log (enemies [enemyNumber].name);
+	}
+
+	void SpawnEnemies()
+	{
+		enemies [0].SetActive (true);
+		enemies [1].SetActive (true);
+		enemies [2].SetActive (true);
+		enemies [3].SetActive (true);
+		enemies [4].SetActive (true);
+		enemies [5].SetActive (true);
+	}
+
+	void KillAllEnemies()
+	{
+		enemies [0].SetActive (false);
+		enemies [1].SetActive (false);
+		enemies [2].SetActive (false);
+		enemies [3].SetActive (false);
+		enemies [4].SetActive (false);
+		enemies [5].SetActive (false);
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.S))
+			SpawnEnemies();
+		
+		if (Input.GetKeyDown (KeyCode.K))
+			KillEnemy (enemyNumb);
+
+		if (Input.GetKeyDown (KeyCode.A))
+			KillAllEnemies ();
+
+		if (Input.GetKeyDown (KeyCode.H))
+			DecrementHealth ();
+
+		if (Input.GetKeyDown (KeyCode.Alpha1))
+			health = MinusHealth(50,2);//Ogre strength
+		
+		if (Input.GetKeyDown (KeyCode.Alpha2))
+			health = MinusHealth(30,2);//Elf strength
+		
+		if (Input.GetKeyDown (KeyCode.Alpha0))
+			health = MinusHealth(10,2);//Dwarf strength
+	}
+}
---
+++ Assets/Week3_1.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Week3_1 : MonoBehaviour {
+
+	public int health;
+	public int mana;		//Mana governs how our magic works
+	public float attackPower;
+
+	public GameObject[] enemies;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//enemies = new GameObject[5];
+		ResetValues ();
+		Debug.Log (enemies.Length);
+
+	}
+	
+	void ResetValues ()
+	{
+		health = 100;
+		mana = 100;
+		attackPower = 1;
+	}
+
+	int DecrementHealth(int power, int modifier)
+	{
+		health -= (power * modifier);
+		return health;
+	}
+
+	void Update()
+	{
+		Controls ();
+
+		//while (health > 0) 
+		//{
+		//	Debug.Log ("Player is Alive");
+		//}
+	}
+
+	void SetEnemies(int enemyNumber)
+	{
+		enemies [enemyNumber].SetActive (false);
+	}
+
+	void EnemiesAlive()
+	{
+		for (int i = 0; i < enemies.Length; i++) 
+		{
+			enemies [i].SetActive (true);
+		}
+
+		//foreach (GameObject go in enemies)
+		//	go.SetActive (true);
+	}
+
+	void Controls()
+	{
+		if (Input.GetKeyDown (KeyCode.H)) 
+		{
+			health = DecrementHealth(10,1);
+		}
+		if (Input.GetKeyDown (KeyCode.J)) 
+		{
+			health = DecrementHealth(20,2);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha0))
+			SetEnemies (0);
+		if (Input.GetKeyDown (KeyCode.Alpha1))
+			SetEnemies (1);
+		if (Input.GetKeyDown (KeyCode.Alpha2))
+			SetEnemies (2);
+		if (Input.GetKeyDown (KeyCode.Alpha3))
+			SetEnemies (3);
+		if (Input.GetKeyDown (KeyCode.Alpha4))
+			SetEnemies (4);
+		if (Input.GetKeyDown (KeyCode.Space))
+			EnemiesAlive();
+	}
+}
